e6702649ab61ccb6ae27c2670f261f346c6c85ef
snipzer <l.arif@laposte.net>
2019-04-01 16:55:12 +0200

    feat:(GithubManagement) Adding logic to extract repository information

--- back/src/service/GithubService.js
+++ back/src/service/GithubService.js
@@ -31,11 +31,55 @@ class GithubService extends BaseService {
         return new Promise((resolve, reject) => {
             this.dao.getOrganizationUsers()
                 .then((response) => {
-                    console.log(response)
-                    console.log(response.data.viewer.membersWithRole)
-                    resolve(response);
+                    const result = this._extractData(response);
+                    resolve(result);
                 }).catch(err => reject(err));
         });
+    }
+
+    _extractData(response) {
+        const result = [];
+        const members = response.data.viewer.membersWithRole.edges;
+        for (let i = 0; i < members.length; i++) {
+            let repositories = members[i].node.repositories.edges;
+            let username = members[i].node.login;
+            for (let j = 0; j < repositories.length; j++) {
+                let reponame = repositories[j].node.name;
+                let stargazer = repositories[j].node.stargazers.totalCount;
+                this._getRepositoriesInformation(result, reponame, username, stargazer);
+            }
+        }
+        return result;
+    }
+
+    _getRepositoriesInformation(result, reponame, username, stargazer) {
+        let isPresent = false;
+        result.forEach(objet => {
+            if (objet.reponame === reponame) {
+                isPresent = true;
+                if (!this._checkIfUserNamePresent(objet, username)) {
+                    objet.users.push(username);
+                }
+            }
+        });
+        if (!isPresent) {
+            result.push({
+                reponame: reponame,
+                stargazer: stargazer,
+                users: [username]
+            })
+        }
+    }
+
+    _checkIfUserNamePresent(objet, username) {
+        let isUserNamePresent = false;
+        for (let y = 0; y < objet.users.length; y++) {
+            if (objet.users[y] === username) {
+                isUserNamePresent = true;
+                break;
+            }
+        }
+        return isUserNamePresent;
     }
 }
 
